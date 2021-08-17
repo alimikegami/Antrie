fce24e7b63b2d946f40612083e6c4ffd76dc77b3
WayanPande <yande554@gmail.com>
2021-08-17 22:59:39 +0800

    fix alert placement in signUp page

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,19 +1,33 @@
 @extends('layouts/main')
 
-@if (session('alert-success'))
-    <div class="alert alert-success" role="alert">
-        {{ session('alert-success') }}
-    </div>
-@endif
-
-@if (session('alert-danger'))
-    <div class="alert alert-danger" role="alert">
-        {{ session('alert-danger') }}
-    </div>
-@endif
-
 @section('container')
-    <div class="login-page d-flex justify-content-center align-items-center">
+    <div class="login-page d-flex justify-content-center align-items-center flex-column">
+        @if (session('alert-success'))
+            <div class="alert alert-success align-self-center alert-dismissible" role="alert" style="width: 50%">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                    class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img"
+                    aria-label="Warning:">
+                    <path
+                        d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                </svg>
+                {{ session('alert-success') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
+
+        @if (session('alert-danger'))
+            <div class="alert alert-danger align-self-center alert-dismissible" role="alert" style="width: 50%">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                    class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img"
+                    aria-label="Warning:">
+                    <path
+                        d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                    </symbol>
+                </svg>
+                {{ session('alert-danger') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
         <div class="login-content-container d-flex justify-content-center align-items-center">
             <div class="login-content d-flex justify-content-around align-items-center">
                 <div class="login-first-column d-flex flex-column justify-content-center align-items-center">
@@ -27,8 +41,8 @@
                         {{ csrf_field() }}
                         <div class="input-nama">
                             <label for="nama" class="form-label">Nama</label>
-                            <input type="text" class="form-control @error('nama') is-invalid @enderror"
-                                id="nama" name="nama" value="{{ old('nama') }}">
+                            <input type="text" class="form-control @error('nama') is-invalid @enderror" id="nama"
+                                name="nama" value="{{ old('nama') }}">
                             @error('nama')
                                 <div class="invalid-feedback">
                                     Tolong masukkan nama yang valid!
@@ -38,8 +52,8 @@
 
                         <div class="input-email mt-3">
                             <label for="email" class="form-label">Email</label>
-                            <input type="email" class="form-control @error('email') is-invalid @enderror"
-                                id="email" name="email" value="{{ old('email') }}">
+                            <input type="email" class="form-control @error('email') is-invalid @enderror" id="email"
+                                name="email" value="{{ old('email') }}">
                             @error('email')
                                 <div class="invalid-feedback">
                                     {{ $message }}
