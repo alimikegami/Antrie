f6d43504f3010ea4f4e35feee4682804eae7a5a5
Alim Ikegami <alimikegami1@gmail.com>
2021-08-18 14:14:15 +0800

    Fixed bugs caused by the usage of auth

--- app/Http/Controllers/BerandaController.php
+++ app/Http/Controllers/BerandaController.php
@@ -6,18 +6,20 @@ use App\Models\Antrean;
 use App\Models\Kategori;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class BerandaController extends Controller
 {
     // Return view untuk beranda
     public function index() {
-        if (session()->has('ID_pengguna')) {
+        if (Auth::check()) {
             $kategori_populer = Kategori::withCount('antrean')->orderBy('antrean_count', 'desc')->first();
             return view('beranda', [
                 'title' => "Beranda",
                 'antrean' => Antrean::with(['kategori', 'pengguna'])->where('id_kategori', '=', $kategori_populer->id)->get(),
                 'kategori_populer' => $kategori_populer,
                 'kategori' => Kategori::all(),
+                'nama_user' => Auth::user()->nama
             ]);
         } else {
             return redirect()->route('landingpage');
@@ -33,14 +35,13 @@ class BerandaController extends Controller
     }
 
     public function showAntreanku(){
-        if (session()->has('ID_pengguna')) {
+        if (Auth::check()) {
             return view('antreanku', [
                 'title' => "Antreanku",
-                'antrean' => Antrean::with('loket')->where('id_pembuat', '=', session('ID_pengguna'))->get(),
+                'antrean' => Antrean::with('loket')->where('id_pembuat', '=', Auth::id())->get(),
             ]);
         } else {
             return redirect()->route('landingpage');
         }
-        return view('antreanku');
     }
 }
--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -32,7 +32,7 @@
                             id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
                             <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30"
                                 class="rounded-circle">
-                            <span class="d-none d-sm-inline mx-1">{{ session('nama') }}</span>
+                            <span class="d-none d-sm-inline mx-1">{{ $nama_user }}</span>
                         </a>
                         <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
                             <li><a class="dropdown-item" href="#">New project...</a></li>
