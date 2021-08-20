809ac5de570cc20e00de52b81ee6d6e821d3b46e
Alim Ikegami <alimikegami1@gmail.com>
2021-08-20 15:11:07 +0800

    Refactored the footer as a component

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,29 @@
+<div class="footer">
+    <div class="footer-content">
+        <div class="footer-logo">
+            <img src="img/logoAntrielight.png" alt="">
+        </div>
+        <div class="footer-word1">
+            <ul>
+                <p>Kunjungi kami</p>
+                <li>Instagram</li>
+                <li>Twitter</li>
+                <li>Github</li>
+            </ul>
+        </div>
+        <div class="footer-word2">
+            <ul>
+                <p>Company</p>
+                <li>Contact us</li>
+                <li>Our Customer</li>
+            </ul>
+        </div>
+        <div class="footer-word3">
+            <ul>
+                <p>Support</p>
+                <li>Quick Start Guide</li>
+                <li>Customer Support</li>
+            </ul>
+        </div>
+    </div>
+</div>
