690870bd3c865e7c5eeb1713d0fb01a5bad738e1
WayanPande <yande554@gmail.com>
2021-08-27 23:08:58 +0800

    fix modal content arrangement and replacing button and icon style in aturLoket view

--- resources/views/aturLoket.blade.php
+++ resources/views/aturLoket.blade.php
@@ -14,7 +14,7 @@
                             <div class="lokasi-wrapper d-flex align-items-center">
                                 <div class="logo-lokasi d-flex justify-content-center align-items-center">
                                     <div class="gambar">
-                                        <img src="img/logoAntriedark.png" alt="" width="70px">
+                                        <img src="{{ asset('img/logoAntriedark.png') }}" alt="" width="70px">
                                     </div>
                                 </div>
                                 <div class="keterangan-lokasi">
@@ -40,7 +40,7 @@
                                     <div class="lokasi-wrapper d-flex align-items-center">
                                         <div class="logo-lokasi d-flex justify-content-center align-items-center">
                                             <div class="gambar">
-                                                <img src="img/logoAntriedark.png" alt="" width="70px">
+                                                <img src="{{ asset('img/logoAntriedark.png') }}" alt="" width="70px">
                                             </div>
                                         </div>
                                         <div class="keterangan-loket-pemilik">
@@ -87,34 +87,40 @@
     <div class="modal fade" id="modal-konfirmasi-buka-loket" tabindex="-1" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
         <div class="modal-dialog modal-lg modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-body modal-body-loket d-flex flex-column align-items-center justify-content-between">
-                    <div class="exit-modal align-self-end">
-                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal"
-                            aria-label="Close"></button>
-                    </div>
-                    <form action="/buka-loket" method="post" enctype="multipart/form-data">
-                        @csrf
-                        @method('PUT')
+            <div class="modal-content d-flex p-2" style="background-color: #2B2844;">
+                <div class="exit-modal align-self-end">
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal"
+                        aria-label="Close"></button>
+                </div>
+                <form action="/buka-loket" method="post" enctype="multipart/form-data">
+                    @csrf
+                    @method('PUT')
+                    <div class="modal-body modal-body-loket d-flex flex-column align-items-center justify-content-evenly">
+
                         <div class="check-logo mt-5">
-                            <svg width="127" height="127" viewBox="0 0 127 127" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg width="204" height="204" viewBox="0 0 204 204" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
                                 <path
-                                    d="M90.4887 48.1553C90.7816 47.8624 90.7816 47.3876 90.4887 47.0947C90.1958 46.8018 89.7209 46.8018 89.428 47.0947L90.4887 48.1553ZM52.9166 84.6668L52.3862 85.1971C52.6791 85.49 53.154 85.49 53.4469 85.1971L52.9166 84.6668ZM37.5717 68.2613C37.2788 67.9684 36.8039 67.9684 36.511 68.2613C36.2181 68.5542 36.2181 69.0291 36.511 69.322L37.5717 68.2613ZM89.428 47.0947L52.3862 84.1364L53.4469 85.1971L90.4887 48.1553L89.428 47.0947ZM36.511 69.322L52.3862 85.1971L53.4469 84.1364L37.5717 68.2613L36.511 69.322ZM110.375 63.5C110.375 89.3883 89.3883 110.375 63.5 110.375V111.875C90.2168 111.875 111.875 90.2168 111.875 63.5H110.375ZM63.5 110.375C37.6117 110.375 16.625 89.3883 16.625 63.5H15.125C15.125 90.2168 36.7832 111.875 63.5 111.875V110.375ZM16.625 63.5C16.625 37.6117 37.6117 16.625 63.5 16.625V15.125C36.7832 15.125 15.125 36.7832 15.125 63.5H16.625ZM63.5 16.625C89.3883 16.625 110.375 37.6117 110.375 63.5H111.875C111.875 36.7832 90.2168 15.125 63.5 15.125V16.625Z"
+                                    d="M102.75 144.415C102.75 144.001 102.414 143.665 102 143.665C101.586 143.665 101.25 144.001 101.25 144.415H102.75ZM101.25 144.5C101.25 144.914 101.586 145.25 102 145.25C102.414 145.25 102.75 144.914 102.75 144.5H101.25ZM101.25 119C101.25 119.414 101.586 119.75 102 119.75C102.414 119.75 102.75 119.414 102.75 119H101.25ZM75.7759 77.8502C75.668 78.2501 75.9048 78.6617 76.3047 78.7696C76.7046 78.8774 77.1163 78.6407 77.2241 78.2407L75.7759 77.8502ZM101.25 144.415V144.5H102.75V144.415H101.25ZM126.75 84.2273C126.75 92.6662 120.753 96.9028 114.327 101.284C108.026 105.58 101.25 110.042 101.25 119H102.75C102.75 110.958 108.724 106.92 115.173 102.523C121.497 98.2109 128.25 93.5611 128.25 84.2273H126.75ZM102 60.25C115.912 60.25 126.75 71.0023 126.75 84.2273H128.25C128.25 70.1392 116.705 58.75 102 58.75V60.25ZM77.2241 78.2407C80.0051 67.9284 90.2398 60.25 102 60.25V58.75C89.6159 58.75 78.7471 66.8324 75.7759 77.8502L77.2241 78.2407ZM177.75 102C177.75 143.836 143.836 177.75 102 177.75V179.25C144.664 179.25 179.25 144.664 179.25 102H177.75ZM102 177.75C60.1644 177.75 26.25 143.836 26.25 102H24.75C24.75 144.664 59.336 179.25 102 179.25V177.75ZM26.25 102C26.25 60.1644 60.1644 26.25 102 26.25V24.75C59.336 24.75 24.75 59.336 24.75 102H26.25ZM102 26.25C143.836 26.25 177.75 60.1644 177.75 102H179.25C179.25 59.336 144.664 24.75 102 24.75V26.25Z"
                                     fill="#4DB0FF" />
                             </svg>
+
                         </div>
                         <div class="word-modal-no-loket text-center">
-                            <p>Apakah anda yakin untuk membuka loket ini?</p>
+                            <h2>Apakah anda yakin untuk membuka loket ini?</h2>
                         </div>
                         <div class="form-group">
                             <input type="number" class="form-control" id="id_loket_buka" name="id_loket_buka" hidden>
                         </div>
-                        <div class="text-center mt-2">
-                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Batalkan</button>
-                            <button id="submit" type="submit" class="btn btn-success" name="submit">Buka</button>
+                        <div
+                            class="mt-2 d-flex justify-content-evenly aturLoket-modal-btn-row flex-column align-items-center">
+                            <button id="submit" type="submit" class="btn btn-enter-atur-loket" name="submit">Buka
+                                loket</button>
+                            <button type="button" class="btn btn-outline-danger btn-cancel-atur-loket mt-4"
+                                data-bs-dismiss="modal">Batalkan</button>
                         </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
@@ -122,34 +128,39 @@
     <div class="modal fade" id="modal-konfirmasi-tutup-loket" tabindex="-1" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
         <div class="modal-dialog modal-lg modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-body modal-body-loket d-flex flex-column align-items-center justify-content-between">
-                    <div class="exit-modal align-self-end">
-                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal"
-                            aria-label="Close"></button>
-                    </div>
-                    <form action="/tutup-loket" method="post" enctype="multipart/form-data">
-                        @csrf
-                        @method('PUT')
+            <div class="modal-content d-flex p-2" style="background-color: #2B2844;">
+                <div class="exit-modal align-self-end">
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal"
+                        aria-label="Close"></button>
+                </div>
+                <form action="/tutup-loket" method="post" enctype="multipart/form-data">
+                    @csrf
+                    @method('PUT')
+                    <div class="modal-body modal-body-loket d-flex flex-column align-items-center justify-content-evenly">
+
                         <div class="check-logo mt-5">
-                            <svg width="127" height="127" viewBox="0 0 127 127" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg width="204" height="204" viewBox="0 0 204 204" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
                                 <path
-                                    d="M90.4887 48.1553C90.7816 47.8624 90.7816 47.3876 90.4887 47.0947C90.1958 46.8018 89.7209 46.8018 89.428 47.0947L90.4887 48.1553ZM52.9166 84.6668L52.3862 85.1971C52.6791 85.49 53.154 85.49 53.4469 85.1971L52.9166 84.6668ZM37.5717 68.2613C37.2788 67.9684 36.8039 67.9684 36.511 68.2613C36.2181 68.5542 36.2181 69.0291 36.511 69.322L37.5717 68.2613ZM89.428 47.0947L52.3862 84.1364L53.4469 85.1971L90.4887 48.1553L89.428 47.0947ZM36.511 69.322L52.3862 85.1971L53.4469 84.1364L37.5717 68.2613L36.511 69.322ZM110.375 63.5C110.375 89.3883 89.3883 110.375 63.5 110.375V111.875C90.2168 111.875 111.875 90.2168 111.875 63.5H110.375ZM63.5 110.375C37.6117 110.375 16.625 89.3883 16.625 63.5H15.125C15.125 90.2168 36.7832 111.875 63.5 111.875V110.375ZM16.625 63.5C16.625 37.6117 37.6117 16.625 63.5 16.625V15.125C36.7832 15.125 15.125 36.7832 15.125 63.5H16.625ZM63.5 16.625C89.3883 16.625 110.375 37.6117 110.375 63.5H111.875C111.875 36.7832 90.2168 15.125 63.5 15.125V16.625Z"
+                                    d="M102.75 144.415C102.75 144.001 102.414 143.665 102 143.665C101.586 143.665 101.25 144.001 101.25 144.415H102.75ZM101.25 144.5C101.25 144.914 101.586 145.25 102 145.25C102.414 145.25 102.75 144.914 102.75 144.5H101.25ZM101.25 119C101.25 119.414 101.586 119.75 102 119.75C102.414 119.75 102.75 119.414 102.75 119H101.25ZM75.7759 77.8502C75.668 78.2501 75.9048 78.6617 76.3047 78.7696C76.7046 78.8774 77.1163 78.6407 77.2241 78.2407L75.7759 77.8502ZM101.25 144.415V144.5H102.75V144.415H101.25ZM126.75 84.2273C126.75 92.6662 120.753 96.9028 114.327 101.284C108.026 105.58 101.25 110.042 101.25 119H102.75C102.75 110.958 108.724 106.92 115.173 102.523C121.497 98.2109 128.25 93.5611 128.25 84.2273H126.75ZM102 60.25C115.912 60.25 126.75 71.0023 126.75 84.2273H128.25C128.25 70.1392 116.705 58.75 102 58.75V60.25ZM77.2241 78.2407C80.0051 67.9284 90.2398 60.25 102 60.25V58.75C89.6159 58.75 78.7471 66.8324 75.7759 77.8502L77.2241 78.2407ZM177.75 102C177.75 143.836 143.836 177.75 102 177.75V179.25C144.664 179.25 179.25 144.664 179.25 102H177.75ZM102 177.75C60.1644 177.75 26.25 143.836 26.25 102H24.75C24.75 144.664 59.336 179.25 102 179.25V177.75ZM26.25 102C26.25 60.1644 60.1644 26.25 102 26.25V24.75C59.336 24.75 24.75 59.336 24.75 102H26.25ZM102 26.25C143.836 26.25 177.75 60.1644 177.75 102H179.25C179.25 59.336 144.664 24.75 102 24.75V26.25Z"
                                     fill="#4DB0FF" />
                             </svg>
                         </div>
                         <div class="word-modal-no-loket text-center">
-                            <p>Apakah anda yakin untuk menutup loket ini?</p>
+                            <h2>Apakah anda yakin untuk menutup loket ini?</h2>
                         </div>
                         <div class="form-group">
                             <input type="number" class="form-control" id="id_loket_tutup" name="id_loket_tutup" hidden>
                         </div>
-                        <div class="text-center mt-2">
-                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Batalkan</button>
-                            <button id="submit" type="submit" class="btn btn-success" name="submit">Tutup</button>
+                        <div
+                            class="mt-2 d-flex justify-content-evenly aturLoket-modal-btn-row flex-column align-items-center">
+                            <button id="submit" type="submit" class="btn-enter-atur-loket" name="submit">Tutup
+                                loket</button>
+                            <button type="button" class="btn btn-outline-danger btn-cancel-atur-loket mt-4"
+                                data-bs-dismiss="modal">Batalkan</button>
                         </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
