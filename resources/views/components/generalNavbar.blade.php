25b31578d2bac1d5773418678a39b901c9e8c926
Alim Ikegami <alimikegami1@gmail.com>
2021-09-01 22:33:43 +0800

    Fixed some bugs on route

--- resources/views/components/generalNavbar.blade.php
+++ resources/views/components/generalNavbar.blade.php
@@ -1,13 +1,13 @@
 <nav class="navbar sticky-top navbar-expand-lg navbar-light">
   <div class="container">
-      <a class="navbar-brand" href="{{ route('landingpage') }}"><img id="navbar-logo" src="img/logoAntriedark.png" alt=""></a>
+      <a class="navbar-brand" href="/"><img id="navbar-logo" src="img/logoAntriedark.png" alt=""></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
           aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav">
-              <a class="nav-link active" aria-current="page" href="{{ route('landingpage') }}">Home</a>
+              <a class="nav-link active" aria-current="page" href="/">Home</a>
               <a class="nav-link" href="{{ route('signup') }}">Sign Up</a>
               <a class="nav-link" href="{{ route('login') }}">Sign In</a>
           </div>
