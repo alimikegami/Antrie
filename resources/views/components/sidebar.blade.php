c45d815982dfe4a4425a3f6eb093594e91ff7c17
Alim Ikegami <alimikegami1@gmail.com>
2021-09-01 21:19:10 +0800

    Added user authorization for the sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -3,38 +3,42 @@
         <a href="/" class="d-flex align-items-center pb-3 text-decoration-none">
             <img class="img-fluid" src="{{ asset('img/logoAntriedark.png') }}" alt="">
         </a>
-        <div class="flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start">
-            <div class="menu-pengguna d-flex flex-column">
-                <div class="menu-dashboard menu">
-                    <i class="bi bi-house"></i><a href="{{ route('beranda') }}">Beranda</a>
-                </div>
-                <div class="menu-antrian menu">
-                    <i class="bi bi-archive"></i><a href="{{ route('antreanku') }}">Antreanku</a>
-                </div>
-                <div class="menu-pesan menu">
-                    <i class="bi bi-chat-left-text"></i><a href="#">Pesan</a>
-                </div>
-                <div class="menu-profil menu">
-                    <i class="bi bi-person"></i><a href="#">Profil</a>
+        @auth
+            <div class="flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start">
+                <div class="menu-pengguna d-flex flex-column">
+                    <div class="menu-dashboard menu">
+                        <i class="bi bi-house"></i><a href="{{ route('beranda') }}">Beranda</a>
+                    </div>
+                    <div class="menu-antrian menu">
+                        <i class="bi bi-archive"></i><a href="{{ route('antreanku') }}">Antreanku</a>
+                    </div>
+                    <div class="menu-pesan menu">
+                        <i class="bi bi-chat-left-text"></i><a href="#">Pesan</a>
+                    </div>
+                    <div class="menu-profil menu">
+                        <i class="bi bi-person"></i><a href="#">Profil</a>
+                    </div>
                 </div>
             </div>
-        </div>
-        <hr>
-        <div class="dropdown pb-4">
-            <a href="#" class="d-flex align-items-center text-decoration-none dropdown-toggle" id="dropdownUser1"
-                data-bs-toggle="dropdown" aria-expanded="false">
-                <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" class="rounded-circle">
-                <span class="d-none d-sm-inline mx-1">{{ Auth::user()->nama }}</span>
-            </a>
-            <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-                <li><a class="dropdown-item" href="#">New project...</a></li>
-                <li><a class="dropdown-item" href="#">Settings</a></li>
-                <li><a class="dropdown-item" href="#">Profile</a></li>
-                <li>
-                    <hr class="dropdown-divider">
-                </li>
-                <li><a class="dropdown-item" href="/logout">Sign out</a></li>
-            </ul>
-        </div>
+            <hr>
+            <div class="dropdown pb-4">
+                <a href="#" class="d-flex align-items-center text-decoration-none dropdown-toggle" id="dropdownUser1"
+                    data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" class="rounded-circle">
+                    <span class="d-none d-sm-inline mx-1">{{ Auth::user()->nama }}</span>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
+                    <li><a class="dropdown-item" href="#">New project...</a></li>
+                    <li><a class="dropdown-item" href="#">Settings</a></li>
+                    <li><a class="dropdown-item" href="#">Profile</a></li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+                    <li><a class="dropdown-item" href="/logout">Sign out</a></li>
+                </ul>
+            </div>
+        @else
+
+        @endauth
     </div>
 </div>
