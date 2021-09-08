c3d6cf1f2f570e4a14ac85d2d0cb501f2916a10b
WayanPande <yande554@gmail.com>
2021-09-08 13:31:07 +0800

    fix email signUp bug and allantrean div className

--- resources/views/email/signup-email.blade.php
+++ resources/views/email/signup-email.blade.php
@@ -1,7 +1,7 @@
-<div style="width: 100%; background: ##edebe8" >
+<div style="width: 100%; background: ##edebe8">
     <table style="max-width: 600px; border: 1px solid #e3e0de; border-collapse:collapse;" align="center">
         <tr style="text-align: center; border: 1px solid #e3e0de; border-collapse:collapse;">
-            <img align="center" class="center autowidth" src="{{ $message->embed('public\img\logoAntriedark.png') }}" alt="">
+            <img align="center" class="center autowidth" src="{{ $message->embed('img/logoAntriedark.png') }}" alt="">
         </tr>
         <tr style="border: 1px solid #e3e0de; border-collapse:collapse; background:white;">
             <td style="padding: 15px;">
@@ -19,5 +19,3 @@
         </tr>
     </table>
 </div>
-
-
