8bff241b5da89aed2d7ea7d67d803bed0be7fa2c
anangbagus <anangbagus666@gmail.com>
2021-09-10 17:50:21 +0800

    sidebar fix responsive

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -6,7 +6,7 @@
     <div class="container-fluid">
         <div class="row flex-nowrap">
             @include('components/sidebar')
-            <div class="col-auto col-md-12 py-3 col-lg-10">
+            <div class="dashboard-container col-auto col-md-12 py-3 col-lg-10 mt-5 mt-md-0">
                 <div class="dashboard-content d-flex flex-column">
                     <div class="container">
                         <div class="first-row d-flex justify-content-between flex-column flex-lg-row">
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,52 +1,6 @@
 @auth
-    <div class="col-auto col-md-2 col-xl-2 px-sm-2 px-0 bg-white d-flex justify-content-center coba d-md-none d-lg-flex d-none">
-        <div class="d-flex flex-column align-items-center px-3 pt-5 min-vh-100 navbar-dashboard">
-            <a href="/" class="d-flex align-items-center pb-3 text-decoration-none">
-                <img class="img-fluid" src="{{ asset('img/logoAntriedark.png') }}" alt="logo-antrie">
-            </a>
-            <div class="flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start">
-                <div class="menu-pengguna d-flex flex-column">
-                    <div class="menu-dashboard menu">
-                        <i class="bi bi-house"></i><a href="{{ route('beranda') }}">Beranda</a>
-                    </div>
-                    <div class="menu-antrian menu">
-                        <i class="bi bi-archive"></i><a href="{{ route('antreanku') }}">Antreanku</a>
-                    </div>
-                    <div class="menu-pesan menu">
-                        <i class="bi bi-chat-left-text"></i><a href="{{ route('riwayat-antrean') }}">Riwayat</a>
-                    </div>
-                </div>
-            </div>
-            <hr>
-            <div class="dropdown pb-4">
-                <a href="#" class="d-flex align-items-center text-decoration-none dropdown-toggle" id="dropdownUser1"
-                    data-bs-toggle="dropdown" aria-expanded="false">
-                    <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" class="rounded-circle">
-                    <span class="d-none d-sm-inline mx-1">{{ Auth::user()->nama }}</span>
-                </a>
-                <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-                    <li><a class="dropdown-item" href="#">New project...</a></li>
-                    <li><a class="dropdown-item" href="#">Settings</a></li>
-                    <li><a class="dropdown-item" href="#">Profile</a></li>
-                    <li>
-                        <hr class="dropdown-divider">
-                    </li>
-                    <li><a class="dropdown-item" href="{{ route('logout') }}">Sign Out</a></li>
-
-                    {{-- <li>
-                        <form action="{{ route('logout') }}" action="POST">
-                            {{ csrf_field() }}
-
-                            <button class="dropdown-item" type="submit">Sign Out</button>
-                        </form>
-                    </li> --}}
-                </ul>
-            </div>
-        </div>
-    </div>
-
     {{-- side Nav mobile --}}
-    <div class="offcanvas offcanvas-start d-lg-none" id="navbar-responsive">
+    <div class="sidebar sidebar-start" id="navbar-responsive" >
         <div class="sidebar-header d-flex flex-row">
             <div class="sidebar-logo">
                 <a href="/" class="d-flex align-items-center text-decoration-none justify-content-center">
@@ -82,6 +36,7 @@
             <a href="#" class="d-flex align-items-center text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
                 <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" class="rounded-3">
                 <span class="d-sm-inline mx-1">{{ Auth::user()->nama }}</span>
+                <i class="bi bi-gear"></i>
             </a>
             <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
                 <li><a class="dropdown-item" href="#">New project...</a></li>
@@ -96,9 +51,8 @@
     </div>
 
     {{-- humburger menu --}}
-    <div class="col-1 d-lg-none d-flex flex-column align-items-center position-fixed bg-white" id="side-nav">
-        <i class="bi bi-list text-center" data-bs-toggle="offcanvas" data-bs-target="#navbar-responsive"
-            aria-controls="navbar-responsive" style="font-size: 2.5rem; color: #2F2D65;"></i>
+    <div class="col-1 d-md-none d-flex flex-column align-items-start position-fixed bg-white w-100 p-0" id="side-nav">
+        <i class="bi bi-list text-center" id="sidenav_btn" style="width: 55px; font-size: 2rem; color: #2F2D65;"></i>
     </div>
 @else
     <div
