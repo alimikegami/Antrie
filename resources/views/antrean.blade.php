7aca20e71d6549def25e9301a0366b923ae93dd3
Alim Ikegami <alimikegami1@gmail.com>
2021-09-12 09:25:48 +0800

    Translating the attachment to indonesian terms

--- resources/views/antrean.blade.php
+++ resources/views/antrean.blade.php
@@ -38,9 +38,9 @@
                             <div class="deskripsi-attactment mt-3">
                                 <p>{{ $antrean->deskripsi }}</p>
                                 @foreach ($antrean->attachmentAntrean as $item)
-                                    <a class="btn btn-outline-info rounded-pill"
+                                    <a class="btn btn-outline-info rounded-pill me-2"
                                         href="{{ Storage::url('attachment/' . $item->file_path_attachment) }}" download><i
-                                            class="bi bi-download"></i> attactment</a>
+                                            class="bi bi-download"></i> Lampiran</a>
                                     {{-- <a href="{{ Storage::url('attachment/' . $item->file_path_attachment) }}" download>
                                         attactment.pdf
                                     </a> --}}
